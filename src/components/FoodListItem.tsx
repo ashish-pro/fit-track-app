d78fe2b8f5f651dcfe7766e730d4f42a3b36559a
ashish-pro <ashishbit017@outlook.com>
2024-03-19 22:52:59 +0530

    inline style to stylesheet api

--- src/components/FoodListItem.tsx
+++ src/components/FoodListItem.tsx
@@ -1,21 +1,14 @@
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
 import { AntDesign } from '@expo/vector-icons';
 
 const FoodListItem = ({ item }) => {
   return (
     <View
-      style={{
-        backgroundColor: "gainsboro",
-        padding: 10,
-        borderRadius: 5,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
+      style={styles.foodItemContainer}
     >
-      <View style={{ flex: 1, gap: 5 }}>
-        <Text style={{ fontWeight: "bold", fontSize: 16 }}>{item.label}</Text>
-        <Text style={{ color: "dimgray" }}>
+      <View style={styles.textContainer}>
+        <Text style={styles.textLabel}>{item.label}</Text>
+        <Text style={styles.textCal}>
           {item.cal} cal, {item.brand}
         </Text>
       </View>
@@ -24,4 +17,25 @@ const FoodListItem = ({ item }) => {
   );
 };
 
+const styles = StyleSheet.create({
+    foodItemContainer:{
+        backgroundColor: "gainsboro",
+        padding: 10,
+        borderRadius: 5,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    textContainer:{
+        flex: 1, 
+        gap: 5
+    },
+    textLabel:{
+        fontWeight: "bold", 
+        fontSize: 16
+    },
+    textCal:{
+        color: "dimgray" 
+    }
+})
 export default FoodListItem;
